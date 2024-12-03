32a0f67f205504082b6333c60fb2d22f9aa95854
Raviyaa27 <ravinduachintha@gmail.com>
2024-12-03 14:02:59 +0530

    initialize firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,21 @@
+import 'package:e_commerce_app_flutter/firebase_options.dart';
 import 'package:e_commerce_app_flutter/utils/constants/colors.dart';
 import 'package:e_commerce_app_flutter/utils/theme/theme.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 import 'app.dart';
 
-void main() {
+Future<void> main() async {
   // TODO: Add Widgets Binding
   // TODO: Init Local Storage
   // TODO: AWait NAtive SPlash
   // TODO: Initilialize Firebase
+  // await Firebase.initializeApp(
+  //   options: DefaultFirebaseOptions.currentPlatform).then(
+  //     (FirebaseApp value) => Get.put(AuthenticationRepository()),
+  // );
   // TODO: Initialize Authentication
 
 
